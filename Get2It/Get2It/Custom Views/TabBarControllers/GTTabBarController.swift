5ba56858fd8c7c2f723c5fbd61db819243b0c388
John Kouris <jdkouris@gmail.com>
2020-03-28 11:06:34 -0700

    Added custom tab bar controller class

---
+++ Get2It/Get2It/Custom Views/TabBarControllers/GTTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  GTTabBarController.swift
+//  Get2It
+//
+//  Created by John Kouris on 3/28/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class GTTabBarController: UITabBarController {
+    
+}
