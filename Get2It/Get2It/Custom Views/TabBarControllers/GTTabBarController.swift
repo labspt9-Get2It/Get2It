d3199056f7d29f0bca9a520f43f7e95b43eb1184
John Kouris <jdkouris@gmail.com>
2020-03-28 11:13:44 -0700

    Added the HomeVC

--- Get2It/Get2It/Custom Views/TabBarControllers/GTTabBarController.swift
+++ Get2It/Get2It/Custom Views/TabBarControllers/GTTabBarController.swift
@@ -10,4 +10,19 @@ import UIKit
 
 class GTTabBarController: UITabBarController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITabBar.appearance().tintColor = .systemBlue
+        viewControllers = [createHomeNC()]
+    }
+    
+    func createHomeNC() -> UINavigationController {
+        let homeVC = UIViewController()
+        homeVC.title = "Home"
+        homeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 0)
+        
+        return UINavigationController(rootViewController: homeVC)
+    }
+    
 }
---
+++ Get2It/Get2It/Screens/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  Get2It
+//
+//  Created by John Kouris on 3/28/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
