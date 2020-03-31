04fa953c5507445337ad38ae943b46296b444a42
Vici Shaweddy <vshaweddy@gmail.com>
2020-03-30 21:11:09 -0400

    set up the collection view

--- Get2It/Get2It/Screens/TaskListVC.swift
+++ Get2It/Get2It/Screens/TaskListVC.swift
@@ -8,22 +8,125 @@
 
 import UIKit
 
-class TaskListVC: UIViewController {
+class TaskListVC: UIViewController, UICollectionViewDelegate {
+    
+    enum SectionLayoutKind: Int, CaseIterable {
+        case grid, list
+        
+        var columnCount: Int {
+            switch self {
+            case .grid:
+                return 2
+            case .list:
+                return 1
+            }
+        }
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<SectionLayoutKind, Int>! = nil
+    var collectionView: UICollectionView! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureViewController()
+        configureViewController()
+        configureHierarchy()
+        configureDataSource()
     }
-    
+}
+
+extension TaskListVC {
     private func configureViewController() {
-        self.view.backgroundColor = .systemBackground
-        self.navigationController?.navigationBar.prefersLargeTitles = true
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         let addBarButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTaskButtonTapped))
-        self.navigationItem.rightBarButtonItem = addBarButton
+        navigationItem.rightBarButtonItem = addBarButton
     }
     
     @objc func addTaskButtonTapped() {
+        var snapshot = dataSource.snapshot()
+        snapshot.appendItems([5, 6, 7, 8], toSection: .list)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            guard let sectionLayoutKind = SectionLayoutKind(rawValue: sectionIndex) else { return nil }
+            let columns = sectionLayoutKind.columnCount
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let groupHeight = columns == 1 ? NSCollectionLayoutDimension.absolute(44) : NSCollectionLayoutDimension.fractionalWidth(0.2)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            return section
+        }
+        return layout
+    }
+    
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(TextCell.self, forCellWithReuseIdentifier: TextCell.reuseIdentifier)
+        collectionView.register(ListCell.self, forCellWithReuseIdentifier: ListCell.reuseIdentifier)
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<SectionLayoutKind, Int>(collectionView: collectionView) {
+            (collectionView:UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            
+            let section = SectionLayoutKind(rawValue: indexPath.section)!
+            if section == .list {
+                // Get a cell of the desired kind
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListCell.reuseIdentifier, for: indexPath) as? ListCell {
+                    
+                    // Populate the cell with our item description
+                    cell.label.text = "\(identifier)"
+                    
+                    // Return the cell
+                    return cell
+                } else {
+                    fatalError("Can't create new cell")
+                }
+            } else {
+                // Get a cell of the desired kind
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TextCell.reuseIdentifier, for: indexPath) as? TextCell {
+                    
+                    // Populate the cell with our item description
+                    cell.label.text = "\(identifier)"
+                    cell.contentView.backgroundColor = .cyan
+                    cell.contentView.layer.borderColor = UIColor.black.cgColor
+                    cell.contentView.layer.borderWidth = 1
+                    cell.contentView.layer.cornerRadius = section == .grid ? 8 : 0
+                    cell.label.textAlignment = .center
+                    cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+                    
+                    // Return the cell
+                    return cell
+                } else {
+                    fatalError("Can't create new cell")
+                }
+            }
+        }
         
+        // Initial data
+        var snapshot = NSDiffableDataSourceSnapshot<SectionLayoutKind, Int>()
+        snapshot.appendSections([.grid, .list])
+        snapshot.appendItems([1, 2], toSection: .grid)
+
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
 }
