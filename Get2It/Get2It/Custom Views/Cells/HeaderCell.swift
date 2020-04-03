85db3a9c478fd75608c8ee34f5506cbdf0638e64
John Kouris <jdkouris@gmail.com>
2020-04-03 16:18:38 -0700

    Copied over the layout from the task detail

--- Get2It/Get2It/Custom Views/Cells/HeaderCell.swift
+++ Get2It/Get2It/Custom Views/Cells/HeaderCell.swift
@@ -9,5 +9,95 @@
 import UIKit
 
 class HeaderCell: UICollectionViewCell {
+    static let reuseIdentifier = "header-cell-reuse-identifier"
     
+    private lazy var mainStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layoutMargins = UIEdgeInsets(top: 12, left: 8, bottom: 12, right: 8)
+        view.isLayoutMarginsRelativeArrangement = true
+        view.spacing = 8
+        view.distribution = .equalSpacing
+        return view
+    }()
+    
+    private lazy var horizontalStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isLayoutMarginsRelativeArrangement = true
+        view.spacing = 8
+        return view
+    }()
+    
+    private lazy var verticalStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isLayoutMarginsRelativeArrangement = true
+        view.spacing = 8
+        return view
+    }()
+    
+    private lazy var iconImage: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = UIColor(red: 5/255, green: 91/255, blue: 195/255, alpha: 1)
+        view.layer.cornerRadius = 20
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "AvenirNext-Medium", size: 17)
+        label.textColor = .systemBackground
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var numberLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "AvenirNext-Medium", size: 24)
+        label.textColor = .systemBackground
+        label.textAlignment = .right
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupMainStackView()
+        setupHorizontalStackView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupMainStackView() {
+        contentView.addSubview(self.mainStackView)
+        NSLayoutConstraint.activate([
+            mainStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            mainStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    private func setupHorizontalStackView() {
+        mainStackView.addArrangedSubview(horizontalStackView)
+        horizontalStackView.addArrangedSubview(iconImage)
+        horizontalStackView.addArrangedSubview(numberLabel)
+        mainStackView.addArrangedSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            iconImage.heightAnchor.constraint(equalToConstant: 40),
+            iconImage.widthAnchor.constraint(equalTo: iconImage.heightAnchor)
+        ])
+        
+        // placeholder
+        self.titleLabel.text = "Tasks"
+        self.numberLabel.text = "3"
+    }
 }
--- Get2It/Get2It/Screens/HomeVC.swift
+++ Get2It/Get2It/Screens/HomeVC.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class HomeVC: UIViewController {
-
+class HomeVC: UIViewController, UICollectionViewDelegate {
+    
+    enum SectionLayoutKind: Int, CaseIterable {
+        case header, grid, list
+        
+        var columnCount: Int {
+            switch self {
+            case .header:
+                return 1
+            case .grid:
+                return 2
+            case .list:
+                return 1
+            }
+        }
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<SectionLayoutKind, Int>! = nil
+    var collectionView: UICollectionView! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         setupTemporaryButton()
+        configureHierarchy()
+        configureDataSource()
     }
     
     // Temporary to navigate to Task List VC
@@ -29,15 +49,107 @@ class HomeVC: UIViewController {
         self.navigationController?.pushViewController(taskListVC, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            guard let sectionLayoutKind = SectionLayoutKind(rawValue: sectionIndex) else { return nil }
+            let columns = sectionLayoutKind.columnCount
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let groupHeight = columns == 1 ? NSCollectionLayoutDimension.absolute(44) : NSCollectionLayoutDimension.fractionalWidth(1/3)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            return section
+        }
+        return layout
     }
-    */
-
+    
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(HeaderCell.self, forCellWithReuseIdentifier: HeaderCell.reuseIdentifier)
+        collectionView.register(SummaryCell.self, forCellWithReuseIdentifier: SummaryCell.reuseIdentifier)
+        collectionView.register(ListCell.self, forCellWithReuseIdentifier: ListCell.reuseIdentifier)
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<SectionLayoutKind, Int>(collectionView: collectionView) {
+            (collectionView:UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            
+            let section = SectionLayoutKind(rawValue: indexPath.section)!
+            
+            if section == .list {
+                // Get a cell of the desired kind
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListCell.reuseIdentifier, for: indexPath) as? ListCell {
+                    
+                    // Populate the cell with our item description
+                    cell.label.text = "\(identifier)"
+                    
+                    // Return the cell
+                    return cell
+                } else {
+                    fatalError("Can't create new cell")
+                }
+            } else if section == .grid {
+                // Get a cell of the desired kind
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SummaryCell.reuseIdentifier, for: indexPath) as? SummaryCell {
+                    
+                    cell.contentView.backgroundColor = UIColor(red: 44/255, green: 44/255, blue: 46/255, alpha: 1)
+                    cell.contentView.layer.borderColor = UIColor.black.cgColor
+                    cell.contentView.layer.borderWidth = 0.2
+                    cell.contentView.layer.cornerRadius = section == .grid ? 10 : 0
+                    
+                    // Return the cell
+                    return cell
+                } else {
+                    fatalError("Can't create new cell")
+                }
+            } else {
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HeaderCell.reuseIdentifier, for: indexPath) as? HeaderCell {
+                    
+                    cell.contentView.backgroundColor = UIColor(red: 44/255, green: 44/255, blue: 46/255, alpha: 1)
+                    cell.contentView.layer.borderColor = UIColor.black.cgColor
+                    cell.contentView.layer.borderWidth = 0.2
+                    cell.contentView.layer.cornerRadius = section == .grid ? 10 : 0
+                    
+                    // Return the cell
+                    return cell
+                } else {
+                    fatalError("Can't create new cell")
+                }
+            }
+        }
+        
+        // Initial data
+        var snapshot = NSDiffableDataSourceSnapshot<SectionLayoutKind, Int>()
+        snapshot.appendSections([.header, .grid, .list])
+        snapshot.appendItems([1, 2], toSection: .grid)
+        
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
