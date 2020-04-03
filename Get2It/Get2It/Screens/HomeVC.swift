884aec7d8752c0bf2ffe3cdf94d17156151d7029
John Kouris <jdkouris@gmail.com>
2020-04-03 16:45:15 -0700

    Refactored createLayout to be part of the UIHelper file

--- Get2It/Get2It/Screens/HomeVC.swift
+++ Get2It/Get2It/Screens/HomeVC.swift
@@ -10,21 +10,6 @@ import UIKit
 
 class HomeVC: UIViewController, UICollectionViewDelegate {
     
-    enum SectionLayoutKind: Int, CaseIterable {
-        case header, grid, list
-        
-        var columnCount: Int {
-            switch self {
-            case .header:
-                return 1
-            case .grid:
-                return 2
-            case .list:
-                return 1
-            }
-        }
-    }
-    
     var dataSource: UICollectionViewDiffableDataSource<SectionLayoutKind, Int>! = nil
     var collectionView: UICollectionView! = nil
     
@@ -49,29 +34,8 @@ class HomeVC: UIViewController, UICollectionViewDelegate {
         self.navigationController?.pushViewController(taskListVC, animated: true)
     }
     
-    func createLayout() -> UICollectionViewLayout {
-        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            
-            guard let sectionLayoutKind = SectionLayoutKind(rawValue: sectionIndex) else { return nil }
-            let columns = sectionLayoutKind.columnCount
-            
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
-            
-            let groupHeight = columns == 1 ? NSCollectionLayoutDimension.absolute(44) : NSCollectionLayoutDimension.fractionalWidth(1/3)
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
-            
-            let section = NSCollectionLayoutSection(group: group)
-            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
-            return section
-        }
-        return layout
-    }
-    
     func configureHierarchy() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createLayout())
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .systemBackground
         collectionView.register(HeaderCell.self, forCellWithReuseIdentifier: HeaderCell.reuseIdentifier)
--- Get2It/Get2It/Utilities/UIHelper.swift
+++ Get2It/Get2It/Utilities/UIHelper.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2020 John Kouris. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum SectionLayoutKind: Int, CaseIterable {
+    case header, grid, list
+    
+    var columnCount: Int {
+        switch self {
+        case .header:
+            return 1
+        case .grid:
+            return 2
+        case .list:
+            return 1
+        }
+    }
+}
+
+enum UIHelper {
+    static func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            guard let sectionLayoutKind = SectionLayoutKind(rawValue: sectionIndex) else { return nil }
+            let columns = sectionLayoutKind.columnCount
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let groupHeight = columns == 1 ? NSCollectionLayoutDimension.absolute(44) : NSCollectionLayoutDimension.fractionalWidth(1/3)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            return section
+        }
+        return layout
+    }
+}
