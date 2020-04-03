14c27e20f1a86de401a81bead7e4b968aa440f6e
John Kouris <jdkouris@gmail.com>
2020-04-03 16:02:30 -0700

    Added the HeaderCell file

---
+++ Get2It/Get2It/Custom Views/Cells/HeaderCell.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCell.swift
+//  Get2It
+//
+//  Created by John Kouris on 4/3/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UICollectionViewCell {
+    
+}
